a4ded3daec3491481b303187dc6348ee2e82c7fa
jvcmtr <jvictorcmtr@gmail.com>
2023-09-14 09:05:37 -0300

    DONE

--- Palavra.java
+++ Palavra.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Palavra {
 
     public static int getNumeroDeVogais(String palavra){
@@ -14,4 +16,37 @@ public class Palavra {
 
         return i;
     }
+
+    public static String getAllDigitos(String palavra){
+        String list = "";
+        for (char c : palavra.toUpperCase().toCharArray()) {
+            String s = "" + c;
+            try{
+                Integer i = Integer.parseInt(s);
+                list += i;
+            } catch(NumberFormatException  e){
+                continue;
+            }
+        }
+        return list;
+    }
+
+    public static String getDigitoAt(String palavra, int index){
+        String s = " ";
+        try{
+            s = ""+Palavra.getAllDigitos(palavra).charAt(index);
+        } catch (IndexOutOfBoundsException e){}
+        return s;
+    }
+
+    public static boolean isPalindrome(String palavra){
+        int length = palavra.length();
+        for (int i = 0; i < length/2; i++) {
+            if( palavra.charAt(i) != palavra.charAt(length-(i+1)) ){
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
--- Program.java
+++ Program.java
@@ -25,12 +25,35 @@ public class Program{
         System.out.println();
         
         System.out.println("b) A string com todas suas letras em maiúsculo. ");
-        System.out.println("\t" + s.toUpperCase(null));
+        System.out.println("\t" + s.toUpperCase());
         System.out.println();
 
 
         System.out.println("c) O número de vogais da string.  (Criar uma classe utilitária e um método pra isso.)");
         System.out.println("\t" + Palavra.getNumeroDeVogais(s));
+        System.out.println();
+
         
+        System.out.println("d) Se a string digitada começa com \"INF\" (ignorando maiúsculas/minúsculas).");
+        System.out.println("\t" + (s.toUpperCase().indexOf("INF")==0));
+        System.out.println();
+
+        System.out.println("e) Se a string digitada termina com \"NET\" (ignorando maiúsculas/minúsculas). ");
+        System.out.println("\t" + (s.toUpperCase().indexOf("NET") == Math.max( (s.length()-3) , 0) ));
+        System.out.println();
+
+        // Não sei se compreendi o enunciado corretamente
+        System.out.println("f) O número de dígitos (0 a 9) da string. (Utilizar a classe utilitaria e criar um método para isso)");
+        System.out.println("\t" + Palavra.getAllDigitos(s).length());
+        System.out.println();
+
+        System.out.println("g) Os 2 primeiros digitos da String. ");
+        System.out.println("\t[" + Palavra.getDigitoAt(s, 0) + "], [" + Palavra.getDigitoAt(s, 1) + "]");
+        System.out.println();
+
+        System.out.println("h) Se a string é um palíndromo ou não. (Utilizar a classe utilitaria e criar um método para isso) ");
+        System.out.println("\t" + Palavra.isPalindrome(s));
+        System.out.println();
+
     }
 }
